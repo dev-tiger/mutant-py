59009cdf96b1ed0e8294e38633beccdc0ddbb5a2
Andrés Alvarez <andresalvarezs10@gmail.com>
2022-03-05 15:55:34 -0500

    Se actualizan conexiones a base de datos, ya que se tuvo un problema con la cuenta actual de AWS

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
 class ConfigTest:
     FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mutant2022@mutant-test.c0ovqhi5fh7x.us-west-2.rds.amazonaws.com:5432/mutant_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Mutant.2022@mutantdb-test.c1nt4f2g9lht.us-east-1.rds.amazonaws.com:5432/mutantdb'
     APP_HOST = environ.get("APP_HOST")
     APP_PORT = environ.get("HOST_PORT")
     SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
